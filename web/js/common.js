614954f38c2642dc06027d87dcc9e46e0eaf7f1b
saurabhkuhikar <saurabhkuhikar55@gmail.com>
2021-06-19 16:27:55 +0530

    Jscript file

--- web/js/common.js
+++ web/js/common.js
@@ -1,21 +1,32 @@
 $(document).ready(function(){
+	$("#cylinderbooking-cylinder_type").change(function(){
+		calculateTotalAmount();
+	});
+	
 	$("#cylinderbooking-cylinder_quantity").bind('keyup mouseup',function(){
-		var cylinderQuantity = $(this).val();
-		var cylinderType = $("#cylinderbooking-cylinder_type").val();		
-		var token = $("#cylinderbooking-token").val();
-	 	if(cylinderQuantity < 6 ){
-	 		$.ajax({
-	            url: '/cylinder-booking/bill-amount',		
-	            type: 'post',
-	            dataType: 'json',
-	            data: {'cylinderQuantity': cylinderQuantity, 'cylinderType': cylinderType, 'token': token}
-	        }).done(function (response) {
-	            if (response.status == 200) {
-	                $("#cylinderbooking-total_amount").val(response.totalAmount);
-	            }
-	        });
-	    }else{
-	    	$(this).val(cylinderQuantity.substring(0, 1));
-	    }
-	});	
+		calculateTotalAmount();
+	});
+	
 });
+
+function calculateTotalAmount(){
+	var cylinderQuantity = $("#cylinderbooking-cylinder_quantity").val();
+	var cylinderType = $("#cylinderbooking-cylinder_type").val();		
+	var token = $("#cylinderbooking-token").val();
+	if(cylinderQuantity != "" && cylinderType != ""){
+		if(cylinderQuantity < 6 ){
+			$.ajax({
+				url: '/cylinder-booking/bill-amount',		
+				type: 'post',
+				dataType: 'json',
+				data: {'cylinderQuantity': cylinderQuantity, 'cylinderType': cylinderType, 'token': token}
+			}).done(function (response) {
+				if (response.status == 200) {
+					$("#cylinderbooking-total_amount").val(response.totalAmount);
+				}
+			});
+		}else{
+			$(this).val(cylinderQuantity.substring(0, 1));
+		}
+	}	
+}
