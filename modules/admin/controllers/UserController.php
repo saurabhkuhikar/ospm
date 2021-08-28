a015de22b77d4632b0b67edb6b175e62e9da207b
saurabhkuhikar <saurabhkuhikar55@gmail.com>
2021-08-28 19:50:32 +0530

    optimizations

--- modules/admin/controllers/UserController.php
+++ modules/admin/controllers/UserController.php
@@ -35,6 +35,7 @@ class UserController extends Controller
      */
     public function actionIndex()
     {
+        $this->layout = '@app/views/layouts/dashboard';
         $searchModel = new UserSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
@@ -52,6 +53,7 @@ class UserController extends Controller
      */
     public function actionView($id)
     {
+        $this->layout = '@app/views/layouts/dashboard';
         return $this->render('view', [
             'model' => $this->findModel($id),
         ]);
@@ -64,6 +66,7 @@ class UserController extends Controller
      */
     public function actionCreate()
     {
+        $this->layout = '@app/views/layouts/dashboard';
         $model = new User();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
@@ -84,6 +87,7 @@ class UserController extends Controller
      */
     public function actionUpdate($id)
     {
+        $this->layout = '@app/views/layouts/dashboard';
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
@@ -104,9 +108,10 @@ class UserController extends Controller
      */
     public function actionDelete($id)
     {
+        $this->layout = '@app/views/layouts/dashboard';
         $this->findModel($id)->delete();
 
-        return $this->redirect(['index']);
+        return $this->redirect(['dashboard']);
     }
 
     /**
--- modules/admin/views/admin/_form.php
+++ modules/admin/views/admin/_form.php
@@ -1,100 +1,126 @@
 <?php
-
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
+use yii\helpers\ArrayHelper;
+use app\models\States;
+use app\models\Cities;
+use kartik\select2\Select2;
 
-/* @var $this yii\web\View */
-/* @var $model app\modules\admin\models\User */
-/* @var $form yii\widgets\ActiveForm */
+$this->title = 'Add User';
 ?>
-
-<div class="user-form">
-    <?php $form = ActiveForm::begin(); ?>
-        <div class="col-md-2"></div>
-        <div class="col-md-12">
-            <div class="panel">
-                <div class="panel-heading">Add User </div>
-                <div class="panel-body">                                     
+<div class="site-login">
+    <div class="col-md-12">
+        <div class="panel">
+            <div class="panel-heading"><?= Html::encode($this->title) ?></div>
+            <div class="panel-body">
+                <?php if (Yii::$app->session->hasFlash('success')): ?>
+                    <div class="alert alert-success alert-dismissable">
+                        <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                        <?= Yii::$app->session->getFlash('success') ?>
+                    </div>
+                <?php endif; ?>
+                <?php $form = ActiveForm::begin(['id' => 'register-form']); ?>
+                    
                     <div class="row">
                         <div class="col-md-6">
-                            <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
-
+                            <div class="form-group">
+                                <?= $form->field($model, 'first_name')->textInput(['autofocus' => true,'placeholder' => 'First Name']) ?>    
+                            </div>
                         </div>
                         <div class="col-md-6">
-                            <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'last_name')->textInput(['autofocus' => true,'placeholder' => 'Last Name']) ?>    
+                            </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'email')->textInput(['autofocus'=>true,'placeholder'=>'Email']) ?>    
+                            </div>
                         </div>
                         <div class="col-md-6">
-                            <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'password')->passwordInput(['autofocus'=>true,'placeholder'=>'Password']) ?>
+                            </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <?= $form->field($model, 'auth_key')->textInput(['maxlength' => true]) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'phone_number')->textInput(['autofocus'=>true,'placeholder'=>'Phone Number']) ?>
+                            </div>  
                         </div>
                         <div class="col-md-6">
-                            <?= $form->field($model, 'phone_number')->textInput(['maxlength' => true]) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'age')->textInput(['autofocus'=>true,'placeholder'=>'Age']) ?>
+                            </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                             <?= $form->field($model, 'age')->textInput() ?>
+                            <div class="form-group">                                    
+                                <?= $form->field($model, 'gender')->radioList(['male'=>'male', 'female' => 'female'], ['unselect' => null])?>
+                            </div>
                         </div>
                         <div class="col-md-6">
-                            <?= $form->field($model, 'gender')->textInput(['maxlength' => true]) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'address')->textInput(['autofocus'=>true,'placeholder'=>'Enter the Address','autocomplete' => 'offgg']) ?>
+                            </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'state')->widget(Select2::classname(), [
+                                    'data' => ArrayHelper::map(States::find()->all(),'state_name','state_name'),
+                                    'options' => ['placeholder' => 'Select States'],
+                                    'pluginOptions' => ['allowClear' => true],
+                                ]);?> 
+                            </div>
                         </div>
                         <div class="col-md-6">
-                            <?= $form->field($model, 'state')->textInput(['maxlength' => true]) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'city')->widget(Select2::classname(), [
+                                    'data' => ArrayHelper::map(Cities::find()->all(),'city_name','city_name'),                                        
+                                    'options' => ['placeholder' => 'Select Cities'],
+                                    'pluginOptions' => ['allowClear' => true],  
+                                ]);?>   
+                            </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'identity_proof')->textInput(['autofocus'=>true,'placeholder'=>'Identity Proof']) ?>  
+                            </div>
                         </div>
                         <div class="col-md-6">
-                            <?= $form->field($model, 'company_name')->textInput(['maxlength' => true]) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'aadhar_card_number')->textInput(['autofocus'=>true,'placeholder'=>'Enter your Aadharcard Number']) ?>  
+                            </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <?= $form->field($model, 'profile_picture')->textInput(['maxlength' => true]) ?>
-                        </div>
-                        <div class="col-md-6">
-                            <?= $form->field($model, 'identity_proof')->textInput(['maxlength' => true]) ?>
-                        </div>
-                    </div>   
-                    <div class="row">
-                        <div class="col-md-6">
-                            <?= $form->field($model, 'identity_proof_type')->textInput(['maxlength' => true]) ?>
-                        </div>
-                        <div class="col-md-6">
-                            <?= $form->field($model, 'aadhar_card_number')->textInput(['maxlength' => true]) ?>
-                        </div>
-                    </div>   
-                    <div class="row">
-                        <div class="col-md-6">
-                            <?= $form->field($model, 'account_type')->textInput(['maxlength' => true]) ?>
-                        </div>
-                        <div class="col-md-6">
-                            <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
-                        </div>
-                    </div>  
-                    <div class="form-group text-center">
-                        <?= Html::submitButton('Save Details', ['class' => 'btn btn-success']) ?>
+                            <div class="form-group">
+                                <?= $form->field($model, 'account_type')->widget(Select2::classname(), [
+                                    'data' => ['Manager'=>'Manager','Supplier' => 'Supplier', 'Customer' => 'Customer',],                                        
+                                    'options' => ['placeholder' => 'Select Cities'],
+                                    'pluginOptions' => ['allowClear' => true],  
+                                ]);?> 
+                            </div>
+                        </div>                            
                     </div>
- 
-                </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <?= Html::submitButton('Add User', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                        </div>
+                    </div>
+                <?php ActiveForm::end(); ?>
+               
             </div>
         </div>
-    <?php ActiveForm::end(); ?>
-
+    </div>
 </div>
